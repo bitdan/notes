357e74b86f638e77acf9c343185707877c748506
bitdan <abiasnip@gmail.com>
2021-09-26 08:54:36 +0800

    :pencil2:

--- filepush.bat
+++ filepush.bat
@@ -1,12 +1,12 @@
 @echo off
 echo "-------Begin-------"
 git status
-set  /p  msg=请输入提交注释:
+set  /p  msg=Please enter your comment:
 git add .
 git commit -m "%msg%"
 git pull
 git push
-echo 推送成功：【%msg%】
+echo Successful：【%msg%】
 echo "--------End!--------"
 pause
 
